7ce0d7d5fefd6ff7646f82ea858afd98fd112ee2
Lucas Araripe <pedro.lucx@gmail.com>
2024-06-20 06:37:08 -0300

    feature (Template.UI) : Adding vue leaflet to project

--- src/main.ts
+++ src/main.ts
@@ -18,10 +18,22 @@ const vuetify = createVuetify({
   directives,
 })
 
+// Vue Leaflet
+import { LMap, LTileLayer, LMarker, LPopup, LTooltip } from '@vue-leaflet/vue-leaflet';
+import 'leaflet/dist/leaflet.css';
+
 const app = createApp(App)
 
 app.use(createPinia())
+
 app.use(router)
 app.use(vuetify)
+app.use(vuetify)
+
+app.component('l-map', LMap);
+app.component('l-tile-layer', LTileLayer);
+app.component('l-marker', LMarker);
+app.component('l-popup', LPopup);
+app.component('l-tooltip', LTooltip);
 
 app.mount('#app')
